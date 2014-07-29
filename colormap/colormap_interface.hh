b2e95507d43983c7129bababa2acaa8be87e155d
Nunki <sprimont@iasfbo.inaf.it>
2014-07-29 14:55:46 +0200

    v8 code cleaning

--- colormap/colormap_interface.hh
+++ colormap/colormap_interface.hh
@@ -1,3 +1,7 @@
+/*
+- 2003 - 2014 - The Qk/Sadira project - P.G. Sprimont @ INAF, Bologna, IT. 
+ */
+
 #ifndef __COLORMAP_INTERFACE_HH__
 #define __COLORMAP_INTERFACE_HH__
 
@@ -7,15 +11,15 @@
 namespace sadira{
 
   using namespace v8;
-
+  
   void free_stream_buffer(char* b, void*x);
 
   class colormap_interface  : public node::ObjectWrap {
 
-
   public:
-    static void init(v8::Handle<Object> target);
 
+    static void init(Handle<Object> target);
+    
   protected:
 
     static Persistent<FunctionTemplate> s_ct;
@@ -24,21 +28,18 @@ namespace sadira{
     qk::colormap_array<float> scaled_cmap;
     double cuts[2];
 
-    void set_cuts(v8::Handle<v8::Array>& cutsa);
     void rescale_colormap();
-    void set_colormap(v8::Handle<v8::Array>& colormap) ;
 
-    static v8::Handle<Value> set_cuts_func(const v8::Arguments& args);
-    static v8::Handle<v8::Value> set_colormap_func(const v8::Arguments& args);
- 
-
-    static v8::Handle<Value> New(const v8::Arguments& args){
+    static Handle<Value> set_cuts_histo(const Arguments& args);
+    static Handle<Value> set_cuts(const Arguments& args);
+    static Handle<Value> set_colormap(const Arguments& args);
+    
+    static Handle<Value> New(const Arguments& args){
       HandleScope scope;
       return args.This();
     }
-        
+    
   };
-
 }
 
 
