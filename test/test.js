c5c7dad06d051af007b8f88f0710966cdf3dcd31
Nunki <sprimont@iasfbo.inaf.it>
2014-08-20 18:20:26 +0200

    updated test.js

--- test/test.js
+++ test/test.js
@@ -6,17 +6,20 @@
 
 
 var fits=require("../build/Release/fits");
-var f = new fits.file(process.argv[2]);
 
+var f = new fits.file("example.fits"); //The file is automatically opened for reading if the file name is specified on constructor.
+
+
+//can also use that way.
+//f.file_name="example.fits";
+//f.open();
+
+
+//f.file_name=process.argv[2];
 
 console.log("File is " + f.file_name);
 
-//f.file_name="example.fits";
-//f.file_name=process.argv[2];
 
-//f.open();
-
-/*
 
 f.get_headers(function(error, headers){
   
@@ -30,9 +33,6 @@ f.get_headers(function(error, headers){
 });
 
 
-*/
-
-
 f.read_image_hdu(function(error, image){
     
     if(error){
@@ -41,7 +41,8 @@ f.read_image_hdu(function(error, image){
     }
     
     if(image){
-	//var headers=f.get_headers(); console.log("FITS headers : \n" + JSON.stringify(headers, null, 4));
+
+	console.log("Read image : " + image.width() + ", " + image.height()); 
 	
 	var colormap=[ [0,0,0,1,0], [1,0,1,1,.8], [1,.2,.2,1,.9], [1,1,1,1,1] ];
 	var cuts=[0,200];
@@ -51,6 +52,10 @@ f.read_image_hdu(function(error, image){
 	
 	var fs=require("fs"),out;
 
+	//The image.tile() function was initially designed for a image viewer's tile generator but can be used to produce
+	//custom PNG/JPEG.
+
+
 	out = fs.createWriteStream("small.png");
 	out.write(image.tile( { tile_coord :  [2,3], zoom :  10, tile_size : [64,64], type : "png" }));
 	out.end();
@@ -59,8 +64,10 @@ f.read_image_hdu(function(error, image){
 	out.write(image.tile( { tile_coord :  [0,0], zoom :  0, tile_size : [512,512], type : "jpeg" }));
 	out.end();
 	
-//	image.histogram({ nbins: 350, cuts : [23,65] }, function(error, histo){
-	image.histogram({}, function(error, histo){ //By default cuts are set to min,max and nbins to 200
+//	image.histogram({ nbins: 350, cuts : [23,65] }, function(error, histo){ .... 
+//      By default cuts are set to min,max and nbins to 200
+
+	image.histogram({}, function(error, histo){ 
 	    
 	    if(error)
 		console.log("Histo error : " + error);
@@ -71,9 +78,15 @@ f.read_image_hdu(function(error, image){
 	    }
 	});
 	
-	
 	console.log("End of fits callback!");
-	delete f;
+
+        //Get the image binary data into an arraybuffer 
+
+	var ab=image_data.get_data();
+	console.log("image data bytes " + ab.length);
+
+	
+
     }
 
 });
